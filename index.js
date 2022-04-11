cc30f52b49658b858a72e6b9642d5cd6a6136684
Pruthviraj Jadhav <pruthvirajjadhav1@gmail.com>
2022-04-11 12:21:16 +0530

    initial version

---
+++ index.js
@@ -0,0 +1,146 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import gradient from "gradient-string";
+import inquirer from "inquirer";
+import { createSpinner } from "nanospinner";
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    "Let's test your Javascript knowledge ! \n"
+  );
+
+  await sleep();
+  rainbowTitle.stop();
+
+  console.log(`
+    This is a CLI Quiz.
+    If you get any question wrong you ${chalk.bgRed("Loose")}
+    So get all the questions right...
+    `);
+}
+
+async function handleAnswer(isCorrect) {
+  const spinner = createSpinner("Evaluating answer...").start();
+  await sleep();
+
+  if (isCorrect) {
+    spinner.success({
+      text: `Nice work ${playerName}. That's a correct answer` + "\n",
+    });
+  } else {
+    spinner.error({ text: `💀💀💀 Game over, you lose ${playerName}!` });
+    process.exit(1);
+  }
+}
+
+async function askName() {
+  const answer = await inquirer.prompt({
+    name: "player_name",
+    type: "input",
+    message: "What is your name?",
+    default() {
+      return "Player";
+    },
+  });
+  playerName = answer.player_name;
+}
+
+function winner() {
+  console.clear();
+  figlet(`Nice work , ${playerName} ! You won !`, (err, data) => {
+    console.log(gradient.pastel.multiline(data) + "\n");
+
+    console.log(
+      chalk.green(
+        `Programming isn't about what you know; it's about making the command line look cool`
+      )
+    );
+    process.exit(0);
+  });
+}
+
+async function question1() {
+  const answers = await inquirer.prompt({
+    name: "question_1",
+    type: "list",
+    message: "JavaScript was created in 10 days then released on\n",
+    choices: [
+      "May 23rd, 1995",
+      "Nov 24th, 1995",
+      "Dec 4th, 1995",
+      "Dec 17, 1996",
+    ],
+  });
+
+  return handleAnswer(answers.question_1 === "Dec 4th, 1995");
+}
+
+async function question2() {
+  const answers = await inquirer.prompt({
+    name: "question_2",
+    type: "list",
+    message: 'What is x? var x = 1_1 + "1" + Number(1)\n',
+    choices: ["4", '"4"', '"1111"', "69420"],
+  });
+  return handleAnswer(answers.question_2 === '"1111"');
+}
+
+async function question3() {
+  const answers = await inquirer.prompt({
+    name: "question_3",
+    type: "list",
+    message: `What is the first element in the array? ['🐏', '🦙', '🐍'].length = 0\n`,
+    choices: ["0", "🐏", "🐍", "undefined"],
+  });
+
+  return handleAnswer(answers.question_3 === "undefined");
+}
+
+async function question4() {
+  const answers = await inquirer.prompt({
+    name: "question_4",
+    type: "list",
+    message: "Which of the following is NOT a primitive type?\n",
+    choices: [
+      "boolean",
+      "number",
+      "null",
+      "object", // Correct
+    ],
+  });
+  return handleAnswer(answers.question_4 === "object");
+}
+
+async function question5() {
+  const answers = await inquirer.prompt({
+    name: "question_5",
+    type: "list",
+    message:
+      "JS is a high-level single-threaded, garbage-collected,\n" +
+      "interpreted(or just-in-time compiled), prototype-based,\n" +
+      "multi-paradigm, dynamic language with a ____ event loop\n",
+    choices: ["multi-threaded", "non-blocking", "synchronous", "promise-based"],
+  });
+
+  return handleAnswer(answers.question_5 === "non-blocking");
+}
+
+// Run it with top-level await
+console.clear();
+await welcome();
+await askName();
+await question1();
+await question2();
+await question3();
+await question4();
+await question5();
+winner();
+
+// To run this program use " Node . " in cli
